8c501f1fb84d17479d076d4480e3f8a396383366
LolloXp <127421231+LolloXp@users.noreply.github.com>
2023-05-26 16:51:34 +0200

    Aggiunta documentazione doxygen generata

---
+++ html/search/all_7.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['rectangle_0',['Rectangle',['../class_rectangle.html',1,'Rectangle'],['../class_rectangle.html#a8a933e0ebd9e80ce91e61ffe87fd577e',1,'Rectangle::Rectangle()'],['../class_rectangle.html#a39fe503e448b59d2f8eba94b4cb2adf7',1,'Rectangle::Rectangle(float w, float l)'],['../class_rectangle.html#a4121f6f88daff89334d0c051c718f164',1,'Rectangle::Rectangle(const Rectangle &amp;r)']]],
+  ['rectangle_2ecpp_1',['rectangle.cpp',['../rectangle_8cpp.html',1,'']]],
+  ['rectangle_2eh_2',['rectangle.h',['../rectangle_8h.html',1,'']]],
+  ['reset_3',['Reset',['../class_polygon.html#a0049882c6d5d7fda0fed6f9bcb4271d2',1,'Polygon']]],
+  ['rhombus_4',['Rhombus',['../class_rhombus.html',1,'']]],
+  ['rhombus_2eh_5',['rhombus.h',['../rhombus_8h.html',1,'']]],
+  ['righttrap_6',['RightTrap',['../class_right_trap.html',1,'RightTrap'],['../class_right_trap.html#a83c5b4e1a7f919063a11201551f07b98',1,'RightTrap::RightTrap(const RightTrap &amp;X)'],['../class_right_trap.html#a7653347c8e1c8392a017b981458d8d58',1,'RightTrap::RightTrap(float h, float ts, float bs)']]],
+  ['righttrapezoid_7',['RightTrapezoid',['../class_right_trapezoid.html',1,'']]]
+];
---
+++ html/search/all_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['setdim_0',['SetDim',['../class_rectangle.html#a087ae13609267e49b9fb4785e5d032ab',1,'Rectangle']]],
+  ['setlength_1',['SetLength',['../class_rectangle.html#ae34067b7dc962a3f6baa31a45b9ff913',1,'Rectangle']]],
+  ['setwidth_2',['SetWidth',['../class_rectangle.html#aebfe60964fa3eeb8d8c1077e17fafc2d',1,'Rectangle']]]
+];
